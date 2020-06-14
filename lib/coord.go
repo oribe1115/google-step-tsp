b4d93b874cbf4b75c524e4f31f5daeba4abef3d4
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-06-14 20:02:14 +0900

    wip: use Tour

--- lib/coord.go
+++ lib/coord.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -21,14 +20,16 @@ func CreateCoord(id int, x float64, y float64) *Coord {
 	}
 }
 
-func Distance(coordA *Coord, coordB *Coord) float64 {
+func (c *CoordList) Distance(idA int, idB int) float64 {
+	coordA := c.Get(idA)
+	coordB := c.Get(idB)
 	xd := coordA.X - coordB.X
 	yd := coordA.Y - coordB.Y
 	return math.Sqrt(math.Pow(xd, 2) + math.Pow(yd, 2))
 }
 
-func InitCoordList(length int) *CoordList {
-	coordList := make(CoordList, length)
+func InitCoordList() *CoordList {
+	coordList := make(CoordList, 0)
 	return &coordList
 }
 
@@ -36,47 +37,20 @@ func (c *CoordList) Set(coord *Coord) {
 	*c = append(*c, coord)
 }
 
-func (c CoordList) TotalDistance() float64 {
+func (c CoordList) TotalDistance(tour Tour) float64 {
 	result := float64(0)
-	for i := 0; i < len(c)-1; i++ {
-		result += Distance(c[i], c[i+1])
+	for i := 0; i < len(tour)-1; i++ {
+		result += c.Distance(tour[i], tour[i+1])
 	}
-	result += Distance(c[len(c)-1], c[0])
+	result += c.Distance(tour[len(tour)-1], tour[0])
 	return result
 }
 
-func (c *CoordList) Delete(index int) error {
-	if index < 0 || index > len(*c)-1 {
-		return fmt.Errorf("delete out of range. index=%d", index)
-	}
-	old := *c
-	*c = append(old[:index], old[index+1:]...)
-	return nil
+func (c CoordList) Get(id int) *Coord {
+	return c[id]
 }
 
-func (c *CoordList) Pop(index int) (*Coord, error) {
-	if index < 0 || index > len(*c)-1 {
-		return nil, fmt.Errorf("pop out of range. index=%d", index)
-	}
-
-	coord := (*c)[index]
-	err := c.Delete(index)
-	if err != nil {
-		return nil, err
-	}
-
-	return coord, nil
-}
-
-func (c CoordList) Swap(indexA int, indexB int) {
-	c[indexA], c[indexB] = c[indexB], c[indexA]
-}
-
-func (c CoordList) Get(index int) *Coord {
-	return c[index]
-}
-
-func (c CoordList) ShouldSwap(indexA int, indexB int) bool {
+func (c CoordList) ShouldSwap(indexA int, indexB int, tour Tour) bool {
 	if indexA > indexB {
 		indexA, indexB = indexB, indexA
 	}
@@ -92,16 +66,16 @@ func (c CoordList) ShouldSwap(indexA int, indexB int) bool {
 
 	var deletedDist, newDist float64
 	if indexA == 0 && indexB == len(c)-1 {
-		deletedDist = Distance(c[indexA], c[indexA+1]) + Distance(c[indexB-1], c[indexB])
-		newDist = Distance(c[indexB], c[indexA+1]) + Distance(c[indexB-1], c[indexA])
+		deletedDist = c.Distance(tour[indexA], tour[indexA+1]) + c.Distance(tour[indexB-1], tour[indexB])
+		newDist = c.Distance(tour[indexB], tour[indexA+1]) + c.Distance(tour[indexB-1], tour[indexA])
 	} else if indexA+1 == indexB {
-		deletedDist = Distance(c[indexALeft], c[indexA]) + Distance(c[indexB], c[indexBRight])
-		newDist = Distance(c[indexALeft], c[indexB]) + Distance(c[indexA], c[indexBRight])
+		deletedDist = c.Distance(tour[indexALeft], tour[indexA]) + c.Distance(tour[indexB], tour[indexBRight])
+		newDist = c.Distance(tour[indexALeft], tour[indexB]) + c.Distance(tour[indexA], tour[indexBRight])
 	} else {
-		deletedByA := Distance(c[indexALeft], c[indexA]) + Distance(c[indexA], c[indexA+1])
-		deletedByB := Distance(c[indexB-1], c[indexB]) + Distance(c[indexB], c[indexBRight])
-		newByA := Distance(c[indexB-1], c[indexA]) + Distance(c[indexA], c[indexBRight])
-		newByB := Distance(c[indexALeft], c[indexB]) + Distance(c[indexB], c[indexA+1])
+		deletedByA := c.Distance(tour[indexALeft], tour[indexA]) + c.Distance(tour[indexA], tour[indexA+1])
+		deletedByB := c.Distance(tour[indexB-1], tour[indexB]) + c.Distance(tour[indexB], tour[indexBRight])
+		newByA := c.Distance(tour[indexB-1], tour[indexA]) + c.Distance(tour[indexA], tour[indexBRight])
+		newByB := c.Distance(tour[indexALeft], tour[indexB]) + c.Distance(tour[indexB], tour[indexA+1])
 
 		deletedDist = deletedByA + deletedByB
 		newDist = newByA + newByB
